eb40b6941d79bad2d1bb5835c0b0757286f55d50
Leo Nguyen <leonguyen020@gmail.com>
2018-09-19 13:28:02 +0700

    Add webpack.config.js

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-  mongoURI: "mongodb+srv://s3325238:Vietnam2018@cosc2430-deploy-ima35.gcp.mongodb.net/test?retryWrites=true",
+  mongoURI: "mongodb+srv://s3325238:admin12345@cosc2430-deploy-ima35.gcp.mongodb.net/real-estate?retryWrites=true",
 };
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+//webpack.config.js
+var path = require('path');
+var webpack = require('webpack');
+module.exports = {
+ entry: './client/src/index.js',
+ output: {
+  path: path.join(__dirname, 'client'),
+  filename: 'index.js'
+ },
+ module: {
+  loaders: [{
+   test: /.jsx?$/,
+   loader: 'babel-loader',
+   exclude: /node_modules/,
+   query: {
+    presets: ['es2015', 'react']
+   }
+  },
+  {
+   test: /\.css$/,
+   loader: "style-loader!css-loader"
+  }]
+ }
+}
